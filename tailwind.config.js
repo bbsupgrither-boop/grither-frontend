06005688a4d6339498e8a39212d4216359c370d8
bbsupgrither-boop <bbsupgrither-boop@users.noreply.github.com>
2025-09-06 18:42:27 +0300

    build: tailwind v3 + configs; working styles

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx,js,jsx}"
+  ],
+  theme: { extend: {} },
+  plugins: []
+}
